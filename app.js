6880c763d364b28195ea036f5a80bbf583658197
Radha <radhasharma2786@gmail.com>
2023-11-06 21:00:09 +0530

    crud~operations~added

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const port = 8080;
 const path = require('path');
 // let ejs = require('ejs');
 const mongoose = require('mongoose');
-const seedDb= require('./seed.js');
+const seedDb = require('./seed.js');
 const BlogRoutes = require('./routes/blog.js')
 const engine = require('ejs-mate');
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://127.0.0.1:27017/Blogging-App').then(() => {
     console.log('db connected');
@@ -14,13 +15,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/Blogging-App').then(() => {
     console.log("DB Error");
     console.log(err);
 })
+
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
+app.use(methodOverride('_method')) // for sending patch request 
 
 app.set('views', path.join(__dirname, 'views')); // views folder
 app.use(express.static(path.join(__dirname, 'public'))); // public folder
+app.use(express.urlencoded({ extended: true }))  // middleware for post request
 
- app.use(BlogRoutes); // so that har incoming request par check kiya jaye
+app.use(BlogRoutes); // so that har incoming request par check kiya jaye
+
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to our blogging app');
--- routes/blog.js
+++ routes/blog.js
@@ -8,5 +8,51 @@ router.get('/blogs' , async(req,res)=>{
     res.render('blogs/index' , {items});
 })
 
+// to show form for the new blogs
+router.get('/blogs/new',(req,res)=>{
+    res.render('blogs/new');
+})
+
+// to actually add the product
+router.post('/blogs' , async(req,res)=>{
+    console.log('working on post request');
+    console.log(req.body);
+    let {title , img , author , desc} = req.body;
+    await Blog.create({title , img , author , desc})
+    res.redirect('/blogs');
+})
+
+// to show a particular product
+router.get('/blogs/:id', async(req,res)=>{
+    // console.log('inside show route');
+    const {id}= req.params;
+    // console.log(id);
+    let foundblog = await Blog.findById(id);
+    res.render('blogs/show',{foundblog});
+})
+
+// form to edit a product
+router.get('/blogs/:id/edit', async(req,res)=>{
+    const {id} = req.params;
+    let foundblog = await Blog.findById(id);
+    res.render('blogs/edit',{foundblog});
+})
+
+// to actually edit the data in db
+router.patch('/blogs/:id',async(req,res)=>{
+    let {id}= req.params;
+    let {title,img,author,desc} = req.body;
+    await Blog.findByIdAndUpdate(id,{title,img,author,desc})
+    res.redirect(`/blogs/${id}`);
+})
+
+//  to delete the data from db
+
+router.delete('/blogs/:id',async(req,res)=>{
+    let {id} = req.params;
+    await Blog.findByIdAndDelete(id);
+    res.redirect('/blogs');
+})
+
 
 module.exports = router;
